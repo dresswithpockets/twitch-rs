842d19f5b73033e10371cbfaecdec6867cfd60a1
Tristen Horton <tristen@tristenhorton.com>
2017-04-03 01:35:02 -0700

    Introduced Event enum. Event will be used to return a Result whenever a chat even occurs in the channel

--- src/client.rs
+++ src/client.rs
@@ -1,13 +1,24 @@
 
+use message::Message;
+
 pub struct TwitchClient {
-	user: String,
-	auth: String,
-	default_channel: String,
-	logging: bool,
+	pub user: String,
+	pub auth: String,
+	pub default_channel: String,
+	pub logging: bool,
+}
+
+pub enum Event {
+	MessageReceived(Message),
 }
 
 impl TwitchClient {
 	pub fn connect(&self) {
 		// TODO: Connect!
+	}
+
+	pub fn recv_event(&self) -> Result<Event, &str> {
+
+		Err("Not yet implemented")
 	}
 }
