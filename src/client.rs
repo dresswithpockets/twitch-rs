a3ec8cc8e9329b747ffbb2e171969aeaac51c7f5
Tristen Horton <tristen@tristenhorton.com>
2017-04-03 00:24:00 -0700

    added TwitchClient, User, and Message types

---
+++ src/client.rs
@@ -0,0 +1,13 @@
+
+pub struct TwitchClient {
+	user: String,
+	auth: String,
+	default_channel: String,
+	logging: bool,
+}
+
+impl TwitchClient {
+	pub fn connect(&self) {
+		// TODO: Connect!
+	}
+}
