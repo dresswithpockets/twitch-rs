1a04d9ba6f2d0c415ca69c89d217807e9b1149a3
Tristen Horton <tristen@tristenhorton.com>
2017-04-06 03:13:54 -0700

    Added UserState and UserType types for handling user state changes in chat

--- src/user.rs
+++ src/user.rs
@@ -1,4 +1,6 @@
 
+use std::collections::HashMap;
+
 pub struct User {
 	id: u64,
 	display_name: String
@@ -12,4 +14,31 @@ impl User {
 	pub fn display_name(&self) -> &String {
 		&self.display_name
 	}
+}
+
+pub struct UserState {
+	badges: HashMap<String, String>,
+	color: String,
+	display: String,
+	emotes: String,
+	channel: String,
+	subscriber: bool,
+	moderator: bool,
+	user_type: UserType
+}
+
+impl UserState {
+	pub fn from_irc(irc: String) -> UserState {
+		// TODO: determine UserState from IRC message
+		unimplemented!()
+	}
+}
+
+pub enum UserType {
+	Viewer,
+	Moderator,
+	GlobalModerator,
+	Broadcaster,
+	Admin,
+	Staff
 }
