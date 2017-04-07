d06ce44cc66ec08dace37a7207333f12a05da7e0
Tristen Horton <tristen@tristenhorton.com>
2017-04-07 00:21:31 -0700

    added more irc message detection functions
    for use in detecting what kind of message was received by Twitch IRC

--- src/irc.rs
+++ src/irc.rs
@@ -4,6 +4,7 @@ tristen@tristenhorton.com
 2017-04-03 23:50
 */
 
+use message::ChatMessage;
 use channel;
 use regex::Regex;
 
@@ -24,9 +25,10 @@ pub fn detect_new_subscriber(
 		match read_type(irc, chan.name()) {
 			Some(rtype) => {
 
-				if irc.split("!").nth(0).unwrap() == ":twitchnotify" &&
-					rtype == "PRIVMSG" &&
-					(irc.contains("just subscribed!") ||
+				if rtype == "PRIVMSG" &&
+					irc.split("!").nth(0).unwrap_or("") == ":twitchnotify" &&
+					(
+						irc.contains("just subscribed!") ||
 						irc.to_lowercase().contains("just subscriber with twitch prime!")
 					)
 				{
@@ -37,6 +39,148 @@ pub fn detect_new_subscriber(
 		}
 	}
 
+	None
+}
+
+pub fn detect_message_received(
+	irc: &String,
+	channels: &Vec<channel::Channel>
+) -> Option<String> {
+
+	for chan in channels {
+		match read_type(irc, chan.name()) {
+			Some(rtype) => {
+
+				if rtype == "PRIVMSG" &&
+					irc.split("!").nth(0).unwrap_or("") != ":twitchnotify"
+				{
+					return Some(chan.name().clone());
+				}
+			}
+			_ => {}
+		}
+	}
+
+	None
+}
+
+pub fn detect_command_received(
+	bot_username: &String,
+	irc: &String,
+	channels: &Vec<channel::Channel>,
+	emotes: &Vec<String>,
+	replace_emotes: &bool,
+	cmd_identifiers: &Vec<String>
+) -> Option<String> {
+
+	for chan in channels {
+		match read_type(irc, chan.name()) {
+			Some(rtype) => {
+				if rtype == "PRIVMSG" {
+					let chat_msg = ChatMessage::from(
+						bot_username.clone(),
+						irc.clone(),
+						emotes,
+						replace_emotes.clone()
+					);
+					if cmd_identifiers.iter()
+						.map(|x| chat_msg.text().starts_with(x))
+						.any(|x| x)
+					{
+						return Some(chan.name().clone());
+					}
+				}
+			}
+			_ => {}
+		}
+	}
+
+	None
+}
+
+pub fn detect_user_joined(
+	irc: &String,
+	channels: &Vec<channel::Channel>
+) -> Option<String> {
+
+	for chan in channels {
+		match read_type(irc, chan.name()) {
+			Some(rtype) => {
+				if rtype == "PART" {
+					return Some(chan.name().clone());
+				}
+			}
+			_ => {}
+		}
+	}
+
+	None
+}
+
+pub fn detect_moderator_joined(
+	irc: &String,
+	channels: &Vec<channel::Channel>
+) -> Option<String> {
+
+	for chan in channels {
+		match read_type(irc, chan.name()) {
+			Some(rtype) => {
+				if rtype == "MODE" &&
+					irc.contains(" ") &&
+					irc.split(" ").nth(3).unwrap_or("") == "+o"
+				{
+					return Some(chan.name().clone());
+				}
+			}
+			_ => {}
+		}
+	}
+
+	None
+}
+
+pub fn detect_incorrect_login(irc: &String) -> bool {
+	irc.contains(":") &&
+		irc.split(":").nth(2).unwrap_or("") == "Login authentication failed"
+}
+
+pub fn detect_malformed_oauth(
+	irc: &String,
+	channels: &Vec<channel::Channel>
+) -> Option<String> {
+
+	for chan in channels {
+		match read_type(irc, chan.name()) {
+			Some(rtype) => {
+				if rtype == "NOTICE" &&
+					irc.contains("Improperly formatted auth")
+				{
+					return Some(chan.name().clone());
+				}
+			}
+			_ => {}
+		}
+	}
+	None
+}
+
+pub fn detect_host_left(
+	irc: &String,
+	channels: &Vec<channel::Channel>
+) -> Option<String> {
+
+	for chan in channels {
+		match read_type(irc, chan.name()) {
+			Some(rtype) => {
+				if rtype == "NOTICE" &&
+					irc.contains("has gone offline")
+				{
+					return Some(chan.name().clone());
+				}
+			}
+			_ => {}
+		}
+	}
 	None
 }
 
