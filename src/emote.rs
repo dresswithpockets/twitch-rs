f89ddff36e7e1b6d24e0a90a9cfae8459370e237
Tristen Horton <tristen@tristenhorton.com>
2017-04-07 00:20:35 -0700

    Added for future use, not in use at the moment

---
+++ src/emote.rs
@@ -0,0 +1,8 @@
+/*
+Do we need to represent emotes the same way
+TwitchLib does?
+It adds a lot of functionality, so perhaps
+in a later verison it is something we can add.
+
+For now we'll just use the string representation of the emote
+*/
